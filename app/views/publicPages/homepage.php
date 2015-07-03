a95d2f49e960cdb5320f132acf464110b0d951f5
Kevin Washington <kevin@publet.com>
2015-07-03 17:59:01 -0400

    added sections to homepage

--- app/views/publicPages/homepage.php
+++ app/views/publicPages/homepage.php
@@ -1,4 +1,42 @@
-<div>
-    <p>Homepage</p>
-    <a class="btn btn-lg btn-primary">some butotn</a>
+<div id="homepageContainer">
+
+    <div id="homepageHeroSection">
+        <p>This is the hero section</p>
+    </div>
+
+    <div id="homepageQuickTestimonialSection">
+        <p>This is the quick testimonial section</p>
+    </div>
+
+    <div id="homepageWhatIsPubletSection">
+        <p>This is the what is publet section</p>
+    </div>
+
+    <div id="homepageValuePropositionOneSection">
+        <p>This is the value prop 1 section</p>
+    </div>
+
+    <div id="homepageValuePropositionTwoSection">
+        <p>This is the value prop 2 section</p>
+    </div>
+
+    <div id="homepageValuePropositionThreeSection">
+        <p>This is the value prop 3 section</p>
+    </div>
+
+    <div id="homepageMajorTestimonialSection">
+        <p>This is the major testimonial section</p>
+    </div>
+
+    <div id="homepageAskForConversionSection">
+        <p>This is the ask for conversion section</p>
+    </div>
+
+    <div id="homepageLesserConversionSection">
+        <p>This is the less conversion section</p>
+    </div>
+
+    <div id="homepageFooterSection">
+        <p>This is the footer section</p>
+    </div>
 </div>
