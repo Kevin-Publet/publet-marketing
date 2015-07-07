bff65244cbfa25a015d6554f62282effadc04897
Kevin Washington <kevin@publet.com>
2015-07-06 20:27:49 -0400

    prevent horizontal scrolling

--- app/views/publicPages/homepage.php
+++ app/views/publicPages/homepage.php
@@ -1,4 +1,4 @@
-<div id="homepageContainer">
+<div id="homepageContainer" >
 
     <div id="homepageNavBarSection">
         <nav  class="">
@@ -25,94 +25,109 @@
         </nav>
     </div>
 
+    <div class="contentContainer">
+        <div id="homepageHeroSection" >
+            <div id="homepageHeroContentSection">
+                <div id="homepageHeroContentSectionTitle" class="col-lg-8 homepageHeavyFont">
+                    <h1>
+                        <span class="heroUnderline">Create </span><br/>
+                        Digital <span class="heroUnderline">Content</span> <br/>
+                        That <span class="heroUnderline">Converts</span>.
+                    </h1>
+                </div>
+                <div id="homepageHeroContentSectionCTA" class="col-lg-8 homepageHeavyFont">
+                    <a href="#" class="btn btn-lg btn-primary">+ Start your campaign</a>
+                </div>
+                <div id="homepageHeroContentSectionSubCTA" class="col-lg-6">
+                    <a href="#">See it in action</a>
+                </div>
+            </div>
+        </div>
 
-    <div id="homepageHeroSection">
-        <div id="homepageHeroContentSection">
-            <div id="homepageHeroContentSectionTitle" class="col-lg-8 homepageHeavyFont">
-                <h1>
-                    <span class="heroUnderline">Create </span><br/>
-                    Digital <span class="heroUnderline">Content</span> <br/>
-                    That <span class="heroUnderline">Converts</span>.
-                </h1>
+
+
+
+        <div id="homepageQuickTestimonialSection" class="text-center">
+            <div id="homepageQuickTestimonialSectionPersonWords">
+                <h2 class="homepageHeavyFont">
+                    "We're huge fans of Publet at Contently.<br/>
+                    We see huge potential for Publet to change how brands create digital content in
+                    the future."
+                </h2>
             </div>
-            <div id="homepageHeroContentSectionCTA" class="col-lg-8 homepageHeavyFont">
-                <a href="#" class="btn btn-lg btn-primary">+ Start your campaign</a>
+            <div id="homepageQuickTestimonialSectionPersonImage">
+                <img src="http://placehold.it/150" class="img-circle img-responsive center-block">
             </div>
-            <div id="homepageHeroContentSectionSubCTA" class="col-lg-6">
-                <a href="#">See it in action</a>
+            <div id="homepageQuickTestimonialSectionPersonBio" class="homepageLightFont">
+                <h3>John Smith - Content Director, Contently</h3>
             </div>
+        </div>
+
+        <div id="homepageWhatIsPubletSection" >
+            <h2 class="homepageHeavyFont col-lg-12">What is Publet?</h2>
+            <h3 class="homepageLightFont col-lg-12">
+                Publet helps businesses gain new customers
+            </h3>
+            <h3 class="homepageLightFont col-lg-12 lessTopMargin">
+                by simplifying three phases of content marketing:
+            </h3>
+
+            <div class="row text-center">
+                <div class="col-lg-4">
+                    <h2 class="homepageHeavyFont">Content Creation</h2>
+                    <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
+                </div>
+                <div class="col-lg-4">
+                    <h2 class="homepageHeavyFont">Content Distribution</h2>
+                    <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
+                </div>
+                <div class="col-lg-4">
+                    <h2 class="homepageHeavyFont">Reader Conversion</h2>
+                    <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
+                </div>
+            </div>
+        </div>
+
+        <div id="homepageValuePropositionOneSection" >
+            <div id="homepageValuePropositionVideoSection" class="col-lg-6">
+
+
+            </div>
+            <div id="homepageValuePropositionOneTitle" class="col-lg-6">
+
+            </div>
+            <div >
+                <div id="homepageValuePropositionOneFulfillments" class="col-lg-6">
+
+                </div>
+            </div>
+
+        </div>
+
+        <div id="homepageValuePropositionTwoSection">
+            <p>This is the value prop 2 section</p>
+        </div>
+
+        <div id="homepageValuePropositionThreeSection">
+            <p>This is the value prop 3 section</p>
+        </div>
+
+        <div id="homepageMajorTestimonialSection">
+            <p>This is the major testimonial section</p>
+        </div>
+
+        <div id="homepageAskForConversionSection">
+            <p>This is the ask for conversion section</p>
+        </div>
+
+        <div id="homepageEnticingContentSection">
+            <p>This is the enticing content section</p>
+        </div>
+
+        <div id="homepageFooterSection">
+            <p>This is the footer section</p>
         </div>
     </div>
 
 
-
-
-    <div id="homepageQuickTestimonialSection" class="text-center">
-        <div id="homepageQuickTestimonialSectionPersonWords">
-            <h2 class="homepageHeavyFont">
-                "We're huge fans of Publet at Contently.<br/>
-                We see huge potential for Publet to change how brands create digital content in
-                the future."
-            </h2>
-        </div>
-        <div id="homepageQuickTestimonialSectionPersonImage">
-            <img src="http://placehold.it/150" class="img-circle img-responsive center-block">
-        </div>
-        <div id="homepageQuickTestimonialSectionPersonBio" class="homepageLightFont">
-            <h3>John Smith - Content Director, Contently</h3>
-        </div>
-    </div>
-
-    <div id="homepageWhatIsPubletSection" >
-        <h2 class="homepageHeavyFont col-lg-12">What is Publet?</h2>
-        <h3 class="homepageLightFont col-lg-12">
-            Publet helps businesses <span class="contentUnderline">gain new customers</span>
-        </h3>
-        <h3 class="homepageLightFont col-lg-12 lessTopMargin">
-            by <span class="contentUnderline">simplifying</span> three phases of <span class="contentUnderline">content marketing</span>:
-        </h3>
-
-        <div class="row text-center">
-            <div class="col-lg-4">
-                <h2 class="homepageHeavyFont">Creation</h2>
-                <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
-            </div>
-            <div class="col-lg-4">
-                <h2 class="homepageHeavyFont">Distribution</h2>
-                <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
-            </div>
-            <div class="col-lg-4">
-                <h2 class="homepageHeavyFont">Conversion</h2>
-                <img src="http://placehold.it/300" class="img-circle img-responsive center-block">
-            </div>
-        </div>
-    </div>
-
-    <div id="homepageValuePropositionOneSection">
-        <p>This is the value prop 1 section</p>
-    </div>
-
-    <div id="homepageValuePropositionTwoSection">
-        <p>This is the value prop 2 section</p>
-    </div>
-
-    <div id="homepageValuePropositionThreeSection">
-        <p>This is the value prop 3 section</p>
-    </div>
-
-    <div id="homepageMajorTestimonialSection">
-        <p>This is the major testimonial section</p>
-    </div>
-
-    <div id="homepageAskForConversionSection">
-        <p>This is the ask for conversion section</p>
-    </div>
-
-    <div id="homepageEnticingContentSection">
-        <p>This is the enticing content section</p>
-    </div>
-
-    <div id="homepageFooterSection">
-        <p>This is the footer section</p>
-    </div>
 </div>
