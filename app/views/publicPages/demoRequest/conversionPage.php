720a0b2daa63d31c14734f54a97d9a43d8e18a0d
Kevin Washington <kevin@publet.com>
2015-07-14 11:23:24 -0400

    added http post request functionality to demo request flow and dependencies

--- app/views/publicPages/demoRequest/conversionPage.php
+++ app/views/publicPages/demoRequest/conversionPage.php
@@ -3,12 +3,43 @@
 
     <div id="demoRequestConversionPageHeader">
         <div id="demoRequestConversionPageHeaderTitle">
-            <h2>This is the title</h2> {{tester}}
+            <h2>This is the conversion page for Demo requests</h2>
         </div>
         <div id="demoRequestConversionPageHeaderSubTitle">
-            <h4>This is the subtitle</h4>
+            <h4>This is the subtitle of the page</h4>
         </div>
     </div>
 
-    <a href="/demo-request-confirmed" class="btn btn-primary">Click</a>
+    <div id="demoRequestForm">
+        <form class="form-horizontal" name="demoRequestForm">
+
+            <div class="form-group">
+                <label for="productService" class="text-center"> </label>
+                <input type="text" ng-model="demoRequestProductService" class="form-control input-lg" name="productService" id="productService" placeholder="Description of your company's product or service">
+            </div>
+
+            <div class="form-group">
+                <label for="targetAudience" class="text-center"> </label>
+                <input type="text" ng-model="demoRequestTargetAudience" class="form-control input-lg" name="targetAudience" id="targetAudience" placeholder="Description of your company's target audience">
+            </div>
+
+            <div class="form-group">
+                <label for="companyName" class="text-center">Company's name: </label>
+                <input type="text" ng-model="demoRequestCompanyName" class="form-control input-lg" name="companyName" id="companyName" placeholder="Aamaco">
+            </div>
+
+            <div class="form-group">
+                <label for="name" class="text-center">Enter your name: </label>
+                <input type="text" ng-model="demoRequestName" class="form-control input-lg" name="name" id="name" placeholder="Carl Winslow">
+            </div>
+
+            <div class="form-group">
+                <label for="email" class="text-center">Enter your email: </label>
+                <input type="email" ng-model="demoRequestEmail" class="form-control input-lg" name="email" id="email" placeholder="carl@familyMatters.com">
+            </div>
+
+        </form>
+    </div>
+
+    <a ng-click="sendDemoRequest(demoRequestProductService, demoRequestTargetAudience, demoRequestCompanyName, demoRequestName, demoRequestEmail)" class="btn btn-primary">Click</a>
 </div>
--- public/angular-bootstrap/angular/demoRequest.js
+++ public/angular-bootstrap/angular/demoRequest.js
@@ -1,5 +1,28 @@
 angular.module('app')
-    .controller('demoRequestController', ['$scope', function($scope)
+    .controller('demoRequestController', ['$scope', '$http', function($scope, $http)
     {
         $scope.tester = 'this is a test';
+
+        $scope.sendDemoRequest = function(productService, targetAudience, companyName, name, email)
+        {
+
+            var data = {
+                name : name,
+                email : email,
+                company : companyName,
+                productService : productService,
+                targetAudience : targetAudience
+            };
+
+
+
+            $http.post('/send-demo-request', data).
+                success(function (data, status, headers, config) {
+                    window.location.pathname = 'demo-request-confirmed';
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.error = data;
+                });
+        }
+
     }]);
