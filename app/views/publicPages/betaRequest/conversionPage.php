5e798b1ce108046a57799e4d108ce1331d07ccc8
Kevin Washington <kevin@publet.com>
2015-07-21 11:34:01 -0400

    some changes to the beta request form

--- app/views/publicPages/betaRequest/conversionPage.php
+++ app/views/publicPages/betaRequest/conversionPage.php
@@ -4,10 +4,10 @@
 
     <div id="betaRequestConversionPageHeader">
         <div id="betaRequestConversionPageHeaderTitle">
-            <h2>This is the conversion page for Demo requests</h2>
+            <h3>Ready to convert more customers with enhanced Content Marketing?</h3>
         </div>
         <div id="betaRequestConversionPageHeaderSubTitle">
-            <h4>This is the subtitle of the page</h4>
+            <h4>Complete the short form below and let's get started!</h4>
         </div>
     </div>
 
@@ -15,32 +15,27 @@
         <form class="form-horizontal" name="betaRequestForm">
 
             <div class="form-group">
-                <label for="productService" class="text-center"> </label>
-                <input type="text" ng-model="betaRequestProductService" class="form-control input-lg" name="productService" id="productService" placeholder="Description of your company's product or service">
+                <label for="targetAudience" class="text-center"> Who is your target audience? (Please separate multiple entries with a comma)</label>
+                <input type="text" ng-model="betaRequestTargetAudience" class="form-control input-lg" name="targetAudience" id="targetAudience" placeholder="Real estate agents, SaaS business owners, PHP Developers">
             </div>
 
             <div class="form-group">
-                <label for="targetAudience" class="text-center"> </label>
-                <input type="text" ng-model="betaRequestTargetAudience" class="form-control input-lg" name="targetAudience" id="targetAudience" placeholder="Description of your company's target audience">
+                <label for="companyName" class="text-center">Your Company's name: </label>
+                <input type="text" ng-model="betaRequestCompanyName" class="form-control input-lg" name="companyName" id="companyName" placeholder="Google, Inc.">
             </div>
 
             <div class="form-group">
-                <label for="companyName" class="text-center">Company's name: </label>
-                <input type="text" ng-model="betaRequestCompanyName" class="form-control input-lg" name="companyName" id="companyName" placeholder="Aamaco">
-            </div>
-
-            <div class="form-group">
-                <label for="name" class="text-center">Enter your name: </label>
-                <input type="text" ng-model="betaRequestName" class="form-control input-lg" name="name" id="name" placeholder="Carl Winslow">
+                <label for="name" class="text-center">Your name: </label>
+                <input type="text" ng-model="betaRequestName" class="form-control input-lg" name="name" id="name" placeholder="Larry Page">
             </div>
 
             <div class="form-group">
                 <label for="email" class="text-center">Enter your email: </label>
-                <input type="email" ng-model="betaRequestEmail" class="form-control input-lg" name="email" id="email" placeholder="carl@familyMatters.com">
+                <input type="email" ng-model="betaRequestEmail" class="form-control input-lg" name="email" id="email" placeholder="boss@google.com">
             </div>
 
         </form>
     </div>
 
-    <a ng-click="sendBetaRequest(betaRequestProductService, betaRequestTargetAudience, betaRequestCompanyName, betaRequestName, betaRequestEmail)" class="btn btn-primary">Click</a>
+    <a ng-click="sendBetaRequest(betaRequestProductService, betaRequestTargetAudience, betaRequestCompanyName, betaRequestName, betaRequestEmail)" class="btn btn-danger">Launch Campaign</a>
 </div>
