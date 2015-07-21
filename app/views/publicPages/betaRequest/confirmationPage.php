fe6ba9c97400a9b7b20f3279dd0bad1372fe5976
Kevin Washington <kevin@publet.com>
2015-07-21 13:50:49 -0400

    added sytling to confirmation page

--- app/views/publicPages/betaRequest/confirmationPage.php
+++ app/views/publicPages/betaRequest/confirmationPage.php
@@ -1,17 +1,17 @@
-<div class="container" id="betaRequestConfirmationContainer">
+<div id="betaRequestConfirmationContainer" >
 
 
-    <div id="betaRequestConfirmationPageHeader" class="text-center">
+    <div id="betaRequestConfirmationPageHeader" class="text-center container">
         <div id="betaRequestConfirmationPageHeaderTitle">
-            <h2>Your account is being created!</h2>
+            <h2 class="homepageHeavyFont">Your account is being created!</h2>
         </div>
         <div id="betaRequestConfirmationPageHeaderSubTitle">
-            <h4>
+            <h4 class="homepageLightFont">
                 We are in Private Beta, which means this account creation process is happening manually.
                 We will notify you at the email you provided when your account is ready.
                 Thank you for our interest in enhancing your content marketing with Publet.
             </h4>
-            <h4>
+            <h4 class="homepageHeavyFont">
                 <br/>- MJ, Founder/CEO
             </h4>
         </div>
