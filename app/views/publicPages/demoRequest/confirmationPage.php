10e52535544949af5d4d4cd1977c95af9a789e99
Kevin Washington <kevin@publet.com>
2015-07-22 16:41:53 -0400

    added content and styling to demo confirmation page

--- app/views/publicPages/demoRequest/confirmationPage.php
+++ app/views/publicPages/demoRequest/confirmationPage.php
@@ -1,12 +1,19 @@
-<div class="container" id="demoRequestConfirmationContainer">
+<div id="demoRequestConfirmationContainer" >
 
 
-    <div id="demoRequestConfirmationPageHeader">
+    <div id="demoRequestConfirmationPageHeader" class="text-center container col-lg-8 col-lg-offset-2">
         <div id="demoRequestConfirmationPageHeaderTitle">
-            <h2>This is the title</h2>
+            <h2 class="homepageHeavyFont">Your demo content is being created!</h2>
         </div>
         <div id="demoRequestConfirmationPageHeaderSubTitle">
-            <h4>This is the subtitle</h4>
+            <h4 class="homepageLightFont">
+                We are in Private Beta, which means this process is happening manually.
+                We will notify you at the email you provided when your demo content is ready.
+                Thank you for our interest in enhancing your content marketing with Publet.
+            </h4>
+            <h4 class="homepageHeavyFont">
+                <br/>- MJ, Founder/CEO
+            </h4>
         </div>
     </div>
 </div>
