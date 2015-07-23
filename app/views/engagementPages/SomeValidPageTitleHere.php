d0c7c2273cae2b8890ac65cab663757908acaa51
Kevin Washington <kevin@publet.com>
2015-07-23 11:27:55 -0400

    some universal styling for engagment content

--- app/views/engagementPages/SomeValidPageTitleHere.php
+++ app/views/engagementPages/SomeValidPageTitleHere.php
@@ -1,1 +1,79 @@
-<div>This is some content</div>
+<div class="engagementContentBulletPoint">
+    <div class="engagementContentBulletPointHeader">
+        <h3 class="homepageHeavyFont">This is the first bullet point</h3>
+    </div>
+    <div class="engagementContentBulletPointText">
+        <p class="homepageLightFont">
+            This is the text to go with the bullet point.
+            I dont know what I will talk about but it should be good.
+            Hopefully so good that visitors will click on the nav and get interested in Publet.
+            Hopefully so good even more that they will set up a demo.
+            Or even ask for beta access.
+            Or if those don't work maybe want to get emailed that special content on the homepage.
+        </p>
+    </div>
+</div>
+
+<div class="engagementContentBulletPoint">
+    <div class="engagementContentBulletPointHeader">
+        <h3 class="homepageHeavyFont">This is the second bullet point</h3>
+    </div>
+    <div class="engagementContentBulletPointText">
+        <p class="homepageLightFont">
+            This is the text to go with the bullet point.
+            I dont know what I will talk about but it should be good.
+            Hopefully so good that visitors will click on the nav and get interested in Publet.
+            Hopefully so good even more that they will set up a demo.
+            Or even ask for beta access.
+            Or if those don't work maybe want to get emailed that special content on the homepage.
+        </p>
+    </div>
+</div>
+
+<div class="engagementContentBulletPoint">
+    <div class="engagementContentBulletPointHeader">
+        <h3 class="homepageHeavyFont">This is the third bullet point</h3>
+    </div>
+    <div class="engagementContentBulletPointText">
+        <p class="homepageLightFont">
+            This is the text to go with the bullet point.
+            I dont know what I will talk about but it should be good.
+            Hopefully so good that visitors will click on the nav and get interested in Publet.
+            Hopefully so good even more that they will set up a demo.
+            Or even ask for beta access.
+            Or if those don't work maybe want to get emailed that special content on the homepage.
+        </p>
+    </div>
+</div>
+
+<div class="engagementContentBulletPoint">
+    <div class="engagementContentBulletPointHeader">
+        <h3 class="homepageHeavyFont">This is the fourth bullet point</h3>
+    </div>
+    <div class="engagementContentBulletPointText">
+        <p class="homepageLightFont">
+            This is the text to go with the bullet point.
+            I dont know what I will talk about but it should be good.
+            Hopefully so good that visitors will click on the nav and get interested in Publet.
+            Hopefully so good even more that they will set up a demo.
+            Or even ask for beta access.
+            Or if those don't work maybe want to get emailed that special content on the homepage.
+        </p>
+    </div>
+</div>
+
+<div class="engagementContentBulletPoint">
+    <div class="engagementContentBulletPointHeader">
+        <h3 class="homepageHeavyFont">This is the fifth bullet point</h3>
+    </div>
+    <div class="engagementContentBulletPointText">
+        <p class="homepageLightFont">
+            This is the text to go with the bullet point.
+            I dont know what I will talk about but it should be good.
+            Hopefully so good that visitors will click on the nav and get interested in Publet.
+            Hopefully so good even more that they will set up a demo.
+            Or even ask for beta access.
+            Or if those don't work maybe want to get emailed that special content on the homepage.
+        </p>
+    </div>
+</div>
